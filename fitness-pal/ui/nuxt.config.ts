8242fe6f82aef42d36b32dcb9774f2bf0b82019c
Ratik Gambhir <ratikgambhir@Mac.ht.home>
2025-08-10 19:39:35 -0500

    added initial UI set up

---
+++ fitness-pal/ui/nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/ui-pro',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils'
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  compatibilityDate: '2025-07-16'
+})
