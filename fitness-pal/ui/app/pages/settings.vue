57f456ccf6bd5e6b0850a80efbe0ef407d41c60e
Ratik Gambhir <ratikgambhir@Ratiks-MacBook-Pro.local>
2025-09-30 17:14:51 -0500

    added settings page

---
+++ fitness-pal/ui/app/pages/settings.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <UDashboardPanel id="settings">
+        <template #header>
+            <UDashboardNavbar title="Settings" :ui="{ right: 'gap-3' }">
+                <template #leading>
+                    <UDashboardSidebarCollapse />
+                </template>
+            </UDashboardNavbar>
+        </template>
+
+        <div class="p-4">
+            <UDashboardSidebarCollapse />
+            <h1 class="text-2xl font-bold mb-4">Settings Page</h1>
+            <p>This is the settings page content.</p>
+        </div>
+    </UDashboardPanel>
+    </template>
